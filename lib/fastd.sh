2ae8d58f888d5e98d996eaa13041113dbcbd6282
Steffen Förster <nemesis@chemnitz.freifunk.net>
2021-01-19 07:13:16 +0000

    enable status socket for fastd

--- lib/fastd.sh
+++ lib/fastd.sh
@@ -8,7 +8,7 @@ fastd_init() {
 
 fastd_start() {
 	for ((i=0; i<$(nproc||echo -n 1); i++)); do
-		fastd -d -c conf/fastd.conf --bind any:$(expr 10000 + $i) --interface mesh-vpn$i
+		fastd -d -c conf/fastd.conf --bind any:$(expr 10000 + $i) --interface mesh-vpn$i --status-socket "/run/fastd-$(expr 10000 + $i).sock";
         done
 }
 
